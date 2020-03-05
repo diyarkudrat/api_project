e416e5c238225ac2bc2b2deb8fb6973adfd4dd15
Diyar Kudrat <dkudrat@gmail.com>
2020-03-05 13:48:02 -0800

    removed unused package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose')
