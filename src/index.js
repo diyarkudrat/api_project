cd262cf411371625de0be8e35c21df6d14ee6d5c
Diyar Kudrat <dkudrat@gmail.com>
2020-03-05 13:54:05 -0800

    changed correct variable in server so db can connect

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose')
-const mongo_uri = process.env.MONGODB_URI
+const mongo_uri = process.env.MONGO_LOCAL_CONN_URL
 mongoose.connect(mongo_uri)
 
 const app = express();
