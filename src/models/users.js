fcef3895aae0720283eb6c16f23d73d57bbc3b7d
Diyar Kudrat <dkudrat@gmail.com>
2020-03-06 12:46:53 -0800

    modified password salting issue

--- src/models/users.js
+++ src/models/users.js
@@ -24,14 +24,11 @@ userSchema.pre('save', function(next) {
   if(!user.isModified || !user.isNew) { // don't rehash if it's an old user
     next();
   } else {
-    bcrypt.hash(user.password, process.env.SALTING_ROUNDS, function(err, hash) {
-      if (err) {
-        console.log('Error hashing password for user', user.name);
-        next(err);
-      } else {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
         next();
-      }
+      });
     });
   }
 });
