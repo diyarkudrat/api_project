9116e74e5af20aaa317b8ccb189defbc6190951f
Diyar Kudrat <dkudrat@gmail.com>
2020-03-06 12:38:17 -0800

    fixed salting undefined issue

--- src/models/users.js
+++ src/models/users.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 
-const environment = process.env.NODE_ENV;
-const stage = require('../config')[environment];
-
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -28,7 +24,7 @@ userSchema.pre('save', function(next) {
   if(!user.isModified || !user.isNew) { // don't rehash if it's an old user
     next();
   } else {
-    bcrypt.hash(user.password, stage.saltingRounds, function(err, hash) {
+    bcrypt.hash(user.password, process.env.SALTING_ROUNDS, function(err, hash) {
       if (err) {
         console.log('Error hashing password for user', user.name);
         next(err);
